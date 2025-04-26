81e016ccd8184cd59051382305445d05cb2ee3b0
Hepjix-hyjxi7-womrat <deyamirza89@gmail.com>
2025-04-26 21:39:30 +0700

    first commit

---
+++ mysql/init/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS dbappointment_service;
+CREATE DATABASE IF NOT EXISTS dbdoctor_service;
+CREATE DATABASE IF NOT EXISTS dbpatient-service_service;
+
+-- Tambahkan database lain jika diperlukan
